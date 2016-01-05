e3dcfa34855594507a9e4e6e1545dcee7cda4c25
Matthijs Kuiper <info@matthijskuiper.nl>
2016-01-05 22:12:06 +0100

    Cart toggle

--- js/hs.js
+++ js/hs.js
@@ -27,4 +27,20 @@ $(function () {
     var scrollTo = $(this).data('scrollto') || 0;
     $window.scrollTo(scrollTo, 500);
   });
+
+  /*
+  * Cart Toggle
+  */
+
+  var $cart = $('.cart');
+
+  $cart.find('> .button').on('click', function (event) {
+    event.preventDefault();
+    $cart.toggleClass('active');
+  });
+
+  $cart.find('> .overlay').on('click', function (event) {
+    event.preventDefault();
+    $cart.removeClass('active');
+  });
 });
